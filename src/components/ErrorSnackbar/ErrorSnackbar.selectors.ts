8a86f86668e213d0e13b837c38019a66f0065378
avkotau <kotov.alexander1@mail.ru>
2023-08-22 17:51:14 +0200

    fix: moved logic of useSelector calls to separate files

---
+++ src/components/ErrorSnackbar/ErrorSnackbar.selectors.ts
@@ -0,0 +1,3 @@
+import { AppRootStateType } from "app/store";
+
+export const selectorError = (state: AppRootStateType) => state.app.error
--- src/components/ErrorSnackbar/ErrorSnackbar.tsx
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
-import {AppRootStateType} from 'app/store'
 import {setAppErrorAC} from 'app/app-reducer'
 import { AlertProps, Snackbar } from '@mui/material'
 import MuiAlert from '@mui/material/Alert';
+import { selectorError } from "components/ErrorSnackbar/ErrorSnackbar.selectors";
 
 const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 	props,
@@ -14,7 +14,7 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
 
 export function ErrorSnackbar() {
 
-    const error = useSelector<AppRootStateType, string | null>(state => state.app.error);
+    const error = useSelector(selectorError);
     const dispatch = useDispatch()
 
     const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
