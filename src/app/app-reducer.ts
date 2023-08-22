4234210f56a191bc7c83d6f483f2bd1a4ae60f56
avkotau <kotov.alexander1@mail.ru>
2023-08-22 16:21:11 +0200

    feat: "translated the project from redux-thunk to toolkit. Add prettier. Add to  tsconfig.json the string "baseUrl": "src","

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,54 @@
+import { Dispatch } from "redux";
+import { authAPI } from "api/todolists-api";
+import { setIsLoggedInAC } from "features/Login/auth-reducer";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+const slice = createSlice({
+  name: "app",
+  initialState: {
+    status: "idle" as RequestStatusType,
+    error: null as string | null,
+    isInitialized: false
+  },
+
+  reducers: {
+    setAppErrorAC: (state, action: PayloadAction<{ error: null | string }>) => {
+      state.error = action.payload.error;
+    },
+    setAppStatusAC: (state, action: PayloadAction<{ status: RequestStatusType }>) => {
+      state.status = action.payload.status;
+    },
+    setAppInitializedAC: (state, action: PayloadAction<{ value: boolean }>) => {
+      state.isInitialized = action.payload.value;
+    }
+  }
+});
+
+
+export const appReducer = slice.reducer;
+export const { setAppErrorAC, setAppInitializedAC, setAppStatusAC } = slice.actions;
+
+export type RequestStatusType = "idle" | "loading" | "succeeded" | "failed"
+export type InitialStateType = {
+  // происходит ли сейчас взаимодействие с сервером
+  status: RequestStatusType
+  // если ошибка какая-то глобальная произойдёт - мы запишем текст ошибки сюда
+  error: string | null
+  // true когда приложение проинициализировалось (проверили юзера, настройки получили и т.д.)
+  isInitialized: boolean
+}
+
+export const initializeAppTC = () => (dispatch: Dispatch) => {
+  authAPI.me().then(res => {
+    if (res.data.resultCode === 0) {
+      dispatch(setIsLoggedInAC({ isLoggedIn: true }));
+    } else {
+
+    }
+
+    dispatch(setAppInitializedAC({ value: true }));
+  });
+};
+
+export type SetAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
---
+++ src/features/Login/auth-reducer.ts
@@ -0,0 +1,54 @@
+import { Dispatch } from "redux";
+import { setAppStatusAC } from "app/app-reducer";
+import { authAPI, LoginParamsType } from "api/todolists-api";
+import { handleServerAppError, handleServerNetworkError } from "utils/error-utils";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { clearDataAC } from "features/TodolistsList/todolists-reducer";
+
+const slice = createSlice({
+  name: "auth",
+  initialState: {
+    isLoggedIn: false
+  },
+
+  reducers: {
+    setIsLoggedInAC: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
+      state.isLoggedIn = action.payload.isLoggedIn; //deleted action.payload;
+    }
+  }
+});
+
+export const authReducer = slice.reducer;
+export const { setIsLoggedInAC } = slice.actions;
+// thunks
+export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }));
+  authAPI.login(data)
+    .then(res => {
+      if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC({ isLoggedIn: true }));
+        dispatch(setAppStatusAC({ status: "succeeded" }));
+      } else {
+        handleServerAppError(res.data, dispatch);
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
+};
+export const logoutTC = () => (dispatch: Dispatch) => {
+  dispatch(setAppStatusAC({ status: "loading" }));
+  authAPI.logout()
+    .then(res => {
+      if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC({ isLoggedIn: false }));
+        dispatch(setAppStatusAC({ status: "succeeded" }));
+        dispatch(clearDataAC())
+      } else {
+        handleServerAppError(res.data, dispatch);
+      }
+    })
+    .catch((error) => {
+      handleServerNetworkError(error, dispatch);
+    });
+};
